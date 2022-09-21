b4c3e02d567a869e11ba921a751e1b17685c26a8
Vishnu P <vishnupothia@gmail.com>
2022-09-21 14:52:34 +0530

    modify join-classroom

--- src/Components/JoinClassroom.js
+++ src/Components/JoinClassroom.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { joinClassroomURL } from "../Utils/constants";
 import { options } from "../Utils/req";
 
 function JoinClassroom(props) {
   const [status, setStatus] = useState("Joining...");
   const auth = useSelector((state) => state.auth);
-  const navigate = useNavigate();
   const searchParams = useSearchParams()[0];
   const classid = searchParams.get("classid");
   useEffect(() => {
-    if (!auth.isLoggedIn && auth.user !== "student") {
-      navigate("/login-signup-student");
+    if (!auth.isLoggedIn || auth.user !== "student") {
+      setStatus("You are not signed in as a student");
     }
 
     const joinClass = async () => {
@@ -22,7 +21,7 @@ function JoinClassroom(props) {
     };
 
     joinClass();
-  }, [auth, navigate, classid]);
+  }, [auth, classid]);
   return (
     <div>
       <h1 style={{ textAlign: "center" }} className="mt-5">
